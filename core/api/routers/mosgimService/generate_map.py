6f36ac443c5edb3b4ab4a68b8f2c4c19a6f9cb23
Kalinka George <xitowzys@gmail.com>
2022-10-13 10:48:50 +0800

    ✨ feat: Added file sending
    Added sending a file locally and to a remote service

--- core/api/routers/mosgimService/generate_map.py
+++ core/api/routers/mosgimService/generate_map.py
@@ -1,22 +1,28 @@
-from fastapi import APIRouter, HTTPException, status, Request
+from fastapi import APIRouter, HTTPException, status, Request, UploadFile, File
 import aiohttp
 from core.api.modules.make_request import make_request
 from core.config.envs import DICT_ENVS
 from loguru import logger
+import io
 
 router = APIRouter()
 
 
-@router.post("/ping")
-async def ping_pong_mosgim(text: str, request: Request):
+@router.post("/mosgim/generate-map")
+async def generate_map(mag_type: str, const: str, request: Request, file: bytes = File()):
 
-    global resp_data, status_code_from_service
+    # global resp_data, status_code_from_service
 
     try:
         resp_data, status_code_from_service = await make_request(
-            url=f'{DICT_ENVS["MOSGIM_SERVICE_URL"]}/ping',
+            url=f'{DICT_ENVS["MOSGIM_SERVICE_URL"]}/mosgim/generate-map',
             method=request.scope['method'].lower(),
-            params={"text": text}
+            # data={"file": (file.filename, file.file, 'application/zip')},
+            # open('report.xls', 'rb')
+
+            data={"file": file},
+            params={"mag_type": mag_type, "const": const}
+            # files={"archive": ("test.zip", file.file)}
         )
 
         logger.debug(f"{resp_data}")
@@ -37,4 +43,4 @@ async def ping_pong_mosgim(text: str, request: Request):
 
     # logger.debug(f"({status_code_from_service}) -> {resp_data}")
 
-    return resp_data
+    return "resp_data"
