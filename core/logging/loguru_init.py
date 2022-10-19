4cfc751d03936cd98b54db88e29c8a7a5b387801
Kalinka George <xitowzys@gmail.com>
2022-10-19 13:42:06 +0800

    ⚙️ chore: The log format has been changed

--- core/logging/loguru_init.py
+++ core/logging/loguru_init.py
@@ -6,11 +6,11 @@ from uvicorn_loguru_integration import run_uvicorn_loguru
 def logger_configuration() -> None:
     # TODO: Добавить создание папки logs если её нету
 
-    # logger.add("./logs/logs.log", format="({time}) {level} {message}",
-    #            level="DEBUG", rotation="10 KB", compression="zip", serialize=True)
-
     logger.remove()
+
+    logger.add("./logs/logs.log", format="({time}) {level} {message}",
+               level="DEBUG", rotation="10 KB", compression="zip", serialize=True)
 
     logger.add(
         sys.stdout, colorize=True,
-        format="(<level>{level}</level>) [<cyan>{file}</cyan>:<cyan>{line}</cyan>] [<green>{time:HH:mm:ss}</green>] ➤ <level>{message}</level>")
+        format="(<level>{level}</level>)({module}) [<cyan>{file}</cyan>:<cyan>{line}</cyan>] [<green>{time:HH:mm:ss}</green>] ➤ <level>{message}</level>")
