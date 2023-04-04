9fd882c9854c8aa194f8188d9a6890110ff412f2
Kalinka George <xitowzys@gmail.com>
2023-04-04 17:59:29 +0800

    ✨ feat: Added cors settings

--- core/rest_api/api.py
+++ core/rest_api/api.py
@@ -5,12 +5,14 @@ import uvicorn
 
 import fastapi_gateway_auto_generate
 from core.config.envs import DICT_ENVS
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class API:
     def __init__(self):
         self.__app: FastAPI = FastAPI(debug=True)
         self.__init_routes()
+        self.__cors_settings()
 
         config: fastapi_gateway_auto_generate.Config = fastapi_gateway_auto_generate.Config(
             fast_api_app=self.__app,
@@ -21,6 +23,19 @@ class API:
 
     def __init_routes(self):
         logger.success("Routers initialized")
+
+    def __cors_settings(self):
+        origins = ["*"]
+
+        self.__app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+        logger.success("Cors settings are installed")
 
     def run_uvicorn_server(self):
         run_uvicorn_loguru(
