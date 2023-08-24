10a046cb8f3568f195148145cc22b1b0256959e6
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-08-24 14:47:14 +0800

    fix: Correct TCPLogstashHandler import

--- core/logging/loguru_init.py
+++ core/logging/loguru_init.py
@@ -3,6 +3,7 @@ import logstash
 from loguru import logger
 from core.config.envs import DICT_ENVS
 from uvicorn_loguru_integration import run_uvicorn_loguru
+from logstash.handler_tcp import TCPLogstashHandler
 
 
 def logger_configuration() -> None:
@@ -11,7 +12,7 @@ def logger_configuration() -> None:
     # logger.add("./logs/logs.log", format="({time}) {level} {message}",
     #            level="DEBUG", rotation="10 KB", compression="zip", serialize=True)
 
-    logger.add(logstash.TCPLogstashHandler(DICT_ENVS["LOGSTASH_HOST"], DICT_ENVS["LOGSTASH_PORT"], version=1, tags="PASS"), serialize=True, level="INFO")
+    logger.add(TCPLogstashHandler(DICT_ENVS["LOGSTASH_HOST"], DICT_ENVS["LOGSTASH_PORT"], version=1, tags="PASS"), serialize=True, level="INFO")
 
     logger.add(
         sys.stdout, colorize=True, level="DEBUG",
