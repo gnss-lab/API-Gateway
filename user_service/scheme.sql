8939cc6f68fe0bcb3a10f70b94216ba1c5aa274f
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-09-21 17:52:31 +0800

    feat: Added SQL schema for access permissions

--- user_service/scheme.sql
+++ user_service/scheme.sql
@@ -11,3 +11,15 @@ CREATE TABLE tokens (
     token VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE services (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE user_services (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    service_id INTEGER REFERENCES services(id) ON DELETE CASCADE,
+    UNIQUE (user_id, service_id)
+);
