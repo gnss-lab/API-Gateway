fe45d1bee237f649f52b8b76f85428a692a49a04
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-09-25 18:53:19 +0800

    feat: Create user_repository & update routers

---
+++ user_service/src/core/repository/user_repository.py
@@ -0,0 +1,98 @@
+from sqlalchemy.orm import Session
+from src.core.database.models import UserModel, TokenModel
+from src.core.database.db import get_db
+from fastapi import HTTPException
+from starlette import status
+
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    async def is_user_exist(self, user: UserModel):
+        """
+        Check if a user with the same username or email already exists.
+
+        :param user: User model instance to check.
+        :type user: UserModel
+        :return: True if a user with the same username or email exists, False otherwise.
+        :rtype: bool
+        """
+        return self.db.query(UserModel).filter(
+            (UserModel.username == user.username) | (UserModel.email == user.email)
+        ).first() is not None
+
+    async def get_user(self, username: str):
+        """
+        Get a user by username.
+
+        :param username: Username of the user to retrieve.
+        :type username: str
+        :return: The user model if found, None otherwise.
+        :rtype: UserModel
+        """
+        return self.db.query(UserModel).filter(UserModel.username == username).first()
+
+    async def create_user(self, user: UserModel):
+        """
+        Create a new user.
+
+        :param user: User model instance to create.
+        :type user: UserModel
+        """
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+
+    async def get_user_token(self, user_id: int):
+        """
+        Get the user's token by user ID.
+
+        :param user_id: The ID of the user.
+        :type user_id: int
+        :return: The user's token if found, None otherwise.
+        :rtype: TokenModel
+        """
+        return self.db.query(TokenModel).filter(TokenModel.user_id == user_id).first()
+
+    async def delete_tokens_by_user_id(self, user_id: int):
+        """
+        Delete user tokens from the database for a given user ID.
+
+        :param user_id: The ID of the user for whom to delete tokens.
+        :type user_id: int
+        """
+        self.db.query(TokenModel).filter(TokenModel.user_id == user_id).delete()
+        self.db.commit()
+
+    async def create_user_token(self, user_id: int, token: str):
+        """
+        Create a new user token in the database.
+
+        :param user_id: The ID of the user for whom to create a token.
+        :type user_id: int
+        :param token: The token to be created.
+        :type token: str
+        """
+        db_token = TokenModel(user_id=user_id, token=token)
+        self.db.add(db_token)
+        self.db.commit()
+        self.db.refresh(db_token)
+
+    async def verify_token(self, token: str):
+        """
+        Verify the validity of a user token.
+
+        :param token: The token to be verified.
+        :type token: str
+        :return: The user associated with the token.
+        :rtype: UserModel
+        :raises HTTPException: If the token is invalid or not associated with any user.
+        """
+        user = self.db.query(UserModel).join(TokenModel).filter(TokenModel.token == token).first()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid user token",
+            )
+        return user
--- user_service/src/routers/user.py
+++ user_service/src/routers/user.py
@@ -1,10 +1,12 @@
 import bcrypt
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from loguru import logger
 
 from src.core.database.db import get_db
 from src.core.database.models import UserModel, TokenModel, ServiceModel, UserServiceModel
-from src.core.database.token_table import create_token_db, delete_token_db, verify_token
+from src.core.repository.token_repository import create_token_db, delete_token_db, verify_token
+from src.core.repository.user_repository import UserRepository
 from src.core.utils.utils import create_jwt_token
 
 from src.dtos.user import (
@@ -20,27 +22,31 @@ async def register_user(user: UserCreateRequest, db: Session = Depends(get_db)):
     """
     Register a new user.
 
-    :param user: User registration request data.
-    :type user: UserCreateRequest
-    :param db: Database session.
-    :type db: Session
-    :return: User registration response.
-    :rtype: UserCreateResponse
+    This endpoint allows you to register a new user in the system.
+
+    :param UserCreateRequest user: Data for user registration.
+    :param Session db: Database session. A dependency created using `get_db`.
+
+    :statuscode 200: Successful registration.
+    :statuscode 400: User with the same username or email already exists.
+    :statuscode 500: Internal server error.
     """
-    existing_user = db.query(UserModel).filter(
-        (UserModel.username == user.username) | (UserModel.email == user.email)
-    ).first()
+    try:
+        user_repository = UserRepository(db)
 
-    if existing_user:
-        raise HTTPException(status_code=400, detail="User with the same username or email already exists")
+        if await user_repository.is_user_exist(user):
+            raise HTTPException(status_code=400, detail="User with the same username or email already exists")
 
-    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
-    hashed_password_str = hashed_password.decode('utf-8')
-    db_user = UserModel(username=user.username, password=hashed_password_str, email=user.email)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return UserCreateResponse(message="User registered")
+        hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+        hashed_password_str = hashed_password.decode('utf-8')
+
+        db_user = UserModel(username=user.username, password=hashed_password_str, email=user.email)
+        await user_repository.create_user(db_user)
+
+        return UserCreateResponse(message="User registered")
+    except Exception as e:
+        logger.error(f"Error during user registration: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @router.post("/login", response_model=UserLoginResponse)
@@ -48,27 +54,34 @@ async def login_user(login_request: UserLoginRequest, db: Session = Depends(get_
     """
     Log in a user.
 
-    :param login_request: User login request data.
-    :type login_request: UserLoginRequest
-    :param db: Database session.
-    :type db: Session
-    :return: User login response.
-    :rtype: UserLoginResponse
+    This endpoint allows you to log in an existing user.
+
+    :param UserLoginRequest login_request: Data for user login.
+    :param Session db: Database session. A dependency created using `get_db`.
+
+    :statuscode 200: Successful login.
+    :statuscode 401: Invalid credentials.
     """
-    user = db.query(UserModel).filter(UserModel.username == login_request.username).first()
+    try:
+        user_repository = UserRepository(db)
 
-    if not user or not bcrypt.checkpw(login_request.password.encode('utf-8'), user.password.encode('utf-8')):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        user = await user_repository.get_user(login_request.username)
 
-    existing_token = db.query(TokenModel).filter(TokenModel.user_id == user.id).first()
+        if not user or not bcrypt.checkpw(login_request.password.encode('utf-8'), user.password.encode('utf-8')):
+            raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    if existing_token:
-        return UserLoginResponse(message="User logged in", token=existing_token.token)
+        existing_token = await user_repository.get_user_token(user.id)
 
-    token = create_jwt_token(user.id)
-    create_token_db(db, user.id, token)
+        if existing_token:
+            return UserLoginResponse(message="User logged in", token=existing_token.token)
 
-    return UserLoginResponse(message="User logged in", token=token)
+        token = create_jwt_token(user.id)
+        await user_repository.create_user_token(user.id, token)
+
+        return UserLoginResponse(message="User logged in", token=token)
+    except Exception as e:
+        logger.error(f"Error during user login: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @router.post("/refresh-token", response_model=UserLoginResponse)
